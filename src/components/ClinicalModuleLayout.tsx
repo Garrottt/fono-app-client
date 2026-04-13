14535b319fa521835840fa84b1b332fd612989fc
Gonzalo Garrote Pérez <gonzalogarroteperez@gmail.com>
2026-04-12 20:21:02 -0400

    feat: demo terminada

--- src/components/ClinicalModuleLayout.tsx
+++ src/components/ClinicalModuleLayout.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, useMemo, useState } from "react"
+import { useMemo, useState } from "react"
+import type { ReactNode } from "react"
 import { Link } from "react-router-dom"
 import type { Patient } from "../types/patient.types"
 
